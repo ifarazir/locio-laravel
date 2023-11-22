8bfb22692a3e0c85611387780257fa62ead5681f
Erfan Mirzaee <erfan.mirzaee8620@gmail.com>
2023-11-22 12:06:54 +0330

    Revert "config filament"
    This reverts commit bef019cf2da6aee2e4d95631d2d0773759b892a2.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-	Model::unguard();
     }
 }
--- config/app.php
+++ config/app.php
@@ -167,8 +167,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
-        // App\Providers\Filament\1PanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
